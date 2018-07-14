3567e26f6af450aae2595bcc30daa1f4cc54e20d
brianlee <blees.email@gmail.com>
2018-07-13 21:47:49 -0400

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -16,8 +18,36 @@ class NewVisitorTest(unittest.TestCase):
 
         # Page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # User is invited to enter a to-do item right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # User types 'Buy peacock feathers' into a text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When user hits enter, page updates and the page lists
+        # '#1: Buy peacock feathers' as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # There is still a text box inviting user to add another item.
+        # User enters 'Use peacock feathers to make a fly'
+        self.fail('Finish the test!')
+
+        # The page updates again, and now shows both items on her list
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
